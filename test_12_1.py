db3d1fbcc8aac66d4dad92b6cb40dbff7b30b3a9
sergg57 <sergg57@list.ru>
2024-12-01 15:52:28 +0300

    Update test_12_1.py

--- test_12_1.py
+++ test_12_1.py
@@ -4,20 +4,20 @@ from runner import Runner
 class RunnerTest(unittest.TestCase):
     def test_walk(self):
         Afina_walker = Runner("Afina")
-        for i in range(10):
+        for _ in range(10):
             Afina_walker.walk()
         self.assertEqual(Afina_walker.distance, 50)
 
     def test_run(self):
         Platon_runner = Runner("Platon")
-        for i in range(10):
+        for _ in range(10):
             Platon_runner.run()
         self.assertEqual(Platon_runner.distance, 100)
 
     def test_challenge(self):
         Afina_runner = Runner("Afina")
         Platon_walker = Runner("Platon")
-        for i in range(10):
+        for _ in range(10):
             Afina_runner.run()
             Platon_walker.walk()
         distance_1 = Afina_runner.distance
